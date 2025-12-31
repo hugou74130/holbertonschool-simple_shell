aec164fd3348cebd5566ffadcaf70056f78095c3
Hugo Ramos <hugoramos1466@gmail.com>
2025-12-31 10:41:36 +0100

    Refactor simple shell implementation to improve functionality and performance

---
+++ simple shell v3/shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_ARGS 64
+#define MAX_PATH 1024
+
+char *get_path(void);
+char *find_command(char *command);
+
+#endif 
---
+++ simple shell v3/simple_shell.c
@@ -0,0 +1,94 @@
+#include "shell.h"
+
+/**
+ * main - Simple shell with PATH support
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	int status;
+	char *argv[MAX_ARGS];
+	char *token;
+	char *command_path;
+	int i;
+	extern char **environ;
+
+	while (1)
+	{
+		/* Display prompt */
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, ":) ", 3);
+
+		/* Read command */
+		nread = getline(&line, &len, stdin);
+
+		/* Handle EOF */
+		if (nread == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			break;
+		}
+
+		/* Remove newline */
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
+
+		/* Skip empty lines */
+		if (strlen(line) == 0)
+			continue;
+
+		/* Parse command line */
+		i = 0;
+		token = strtok(line, " \t");
+		while (token != NULL && i < MAX_ARGS - 1)
+		{
+			argv[i] = token;
+			i++;
+			token = strtok(NULL, " \t");
+		}
+		argv[i] = NULL;
+
+		/* Find command in PATH */
+		command_path = find_command(argv[0]);
+
+		if (command_path == NULL)
+		{
+			/* Command not found - don't fork */
+			fprintf(stderr, "./shell: No such file or directory\n");
+			continue;
+		}
+
+		/* Fork only if command exists */
+		pid = fork();
+
+		if (pid == -1)
+		{
+			perror("./shell");
+			continue;
+		}
+
+		if (pid == 0)
+		{
+			/* Child process */
+			if (execve(command_path, argv, environ) == -1)
+			{
+				perror("./shell");
+				exit(127);
+			}
+		}
+		else
+		{
+			/* Parent process */
+			wait(&status);
+		}
+	}
+
+	free(line);
+	return (0);
+}
