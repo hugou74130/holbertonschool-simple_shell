b13dff7a4694a003fc8b15e77b563eeb0c3a1855
Hugo <hugoramos1466@gmail.com>
2026-01-05 11:27:00 +0100

    Update command parsing logic to improve efficiency and enhance error reporting

---
+++ simple-shell-v5/path.c
@@ -0,0 +1,50 @@
+#include "shell.h"
+
+/**
+ * get_path - Get PATH from environment
+ *
+ * Return: PATH string or NULL
+ */
+
+char *get_path(void)
+{
+	return (getenv("PATH"));
+}
+
+char *find_command(char *command)
+{
+	char *path, *path_copy, *dir;
+	static char full_path[MAX_PATH];
+
+	if (command[0] == '/' || command[0] == '.')
+	{
+		if (access(command, X_OK) == 0)
+			return (command);
+		return (NULL);
+	}
+
+	path = get_path();
+	if (path == NULL)
+		return (NULL);
+
+	path_copy = strdup(path);
+	if (path_copy == NULL)
+		return (NULL);
+
+	dir = strtok(path_copy, ":");
+	while (dir != NULL)
+	{
+		snprintf(full_path, MAX_PATH, "%s/%s", dir, command);
+
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
